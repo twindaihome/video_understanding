342697ebbf6c84c86c73819a07764b53175b4eb4
JessieYoung <35671060+lljieying@users.noreply.github.com>
2019-06-02 00:24:08 +0200

    Add 'title' dataset in Fusion (#18)
    Add 'title' dataset in Fusion
    Adjusted the append sequence of 'title' in `train.py`(move to the last)

--- fusion.py
+++ fusion.py
@@ -22,13 +22,15 @@ col_early = ColumnSelector(cols = range(4))
 '''
 def fusion(X,y):
     print('lets start fusion')
-    col_audio = ColumnSelector(cols= (269, -1))
+    col_title = ColumnSelector(cols= (397, -1))
+    col_audio = ColumnSelector(cols= (269, 397))
     col_video = ColumnSelector(cols= (141,269))
     col_face = ColumnSelector(cols = (13,141))
     col_meta = ColumnSelector(cols = (0,13))
     col_early = ColumnSelector(cols = (0,-1))
 
     clf = LinearSVC(tol=1e-200,max_iter=100000)
+    pipe_title = Pipeline([('col', col_title), ('scale', StandardScaler()), ('clf', clf)])
     pipe_audio = Pipeline([('col',col_audio),('scale',StandardScaler()),('clf',clf)])
     pipe_video = Pipeline([('col',col_video),('scale',StandardScaler()),('clf',clf)])
     pipe_face = Pipeline([('col',col_face),('scale',StandardScaler()),('clf',clf)])
@@ -36,21 +38,29 @@ def fusion(X,y):
     pipe_early = Pipeline([('col',col_early),('scale',StandardScaler()),('clf',clf)])
 
     #### Computing baseline accuracies using single feature type each time and then using their early fusion. ####
+    cross_val_accuracy_title = cross_val_score(pipe_title, X, y, cv=5, scoring='accuracy', n_jobs=-1)
     cross_val_accuracy_audio = cross_val_score(pipe_audio, X, y, cv=5, scoring='accuracy', n_jobs=-1)
     cross_val_accuracy_video  = cross_val_score(pipe_video, X, y, cv=5, scoring='accuracy', n_jobs=-1)
     cross_val_accuracy_face  = cross_val_score(pipe_face, X, y, cv=5, scoring='accuracy', n_jobs=-1)
     cross_val_accuracy_meta = cross_val_score(pipe_meta, X, y, cv=5, scoring='accuracy', n_jobs=-1)
     cross_val_accuracy_early  = cross_val_score(pipe_early, X, y, cv=5, scoring='accuracy', n_jobs=-1)
-    print('audio_accuracy:{}, video_accuracy:{},\n face_accuracy:{}, meta_accuracy:{}, \n early_fusion_accuracy:{}'.format(np.mean(cross_val_accuracy_audio),np.mean(cross_val_accuracy_video),
+    print('title_accuracy:{}, \naudio_accuracy:{}, video_accuracy:{},\n face_accuracy:{}, meta_accuracy:{}, \n early_fusion_accuracy:{}'.format(np.mean(cross_val_accuracy_title), np.mean(cross_val_accuracy_audio),np.mean(cross_val_accuracy_video),
           np.mean(cross_val_accuracy_face),np.mean(cross_val_accuracy_meta),np.mean(cross_val_accuracy_early)))
 
     #### Creating Weighted Majority Voting Classifiers without using the early fusion classfier. ####
-    weights = [cross_val_accuracy_audio.mean(), cross_val_accuracy_video.mean(), cross_val_accuracy_face.mean(),cross_val_accuracy_meta.mean()]
-    eclf = EnsembleVoteClassifier(clfs=[pipe_audio, pipe_video,pipe_face,pipe_meta],weights=weights, voting='hard')
-    cross_val_accuracy_late  = cross_val_score(eclf, X, y, cv=5, scoring='accuracy', n_jobs=-1)
-    print('late_ensemble_accuracy:{}'.format(np.mean(cross_val_accuracy_late)))
-    eclf.fit(X, y)
-    return eclf
+    weights = [cross_val_accuracy_title.mean(), cross_val_accuracy_audio.mean(), cross_val_accuracy_video.mean(),cross_val_accuracy_meta.mean(), cross_val_accuracy_face.mean()]
+    eclf1 = EnsembleVoteClassifier(clfs=[pipe_title, pipe_audio, pipe_video, pipe_meta, pipe_face],weights=weights, voting='hard')
+    eclf2 = EnsembleVoteClassifier(clfs=[pipe_audio, pipe_video, pipe_meta, pipe_face], weights=weights[1:],
+                                   voting='hard')
+    eclf3 = EnsembleVoteClassifier(clfs=[pipe_audio, pipe_video, pipe_meta], weights=weights[1:4],
+                                   voting='hard')
+    cross_val_accuracy_late1  = cross_val_score(eclf1, X, y, cv=5, scoring='accuracy', n_jobs=-1)
+    cross_val_accuracy_late2 = cross_val_score(eclf2, X, y, cv=5, scoring='accuracy', n_jobs=-1)
+    cross_val_accuracy_late3 = cross_val_score(eclf3, X, y, cv=5, scoring='accuracy', n_jobs=-1)
+    print('late_ensemble_accuracy_1:{}, \n late_ensemble_accuracy_2:{}, \nlate_ensemble_accuracy_3:{}'.format(np.mean(cross_val_accuracy_late1),np.mean(cross_val_accuracy_late2),np.mean(cross_val_accuracy_late3)))
+    eclf1.fit(X, y)
+
+    return eclf1
 
 
 '''
